1a3991015058b25029598604ece10dec86e84d2f
Erlaaa <85256163+Erlaaa@users.noreply.github.com>
2025-06-05 15:40:27 +0300

    fixed

--- Main.java
+++ Main.java
@@ -8,72 +8,67 @@ public class Main {
         int expenses = 0;
 
         while (true) {
-            String input;
-
             System.out.println("Выберите операцию и введите её номер: ");
             System.out.println("1. Добавить новый доход");
             System.out.println("2. Добавить новый расход");
             System.out.println("3. Выбрать систему налогообложения");
 
-            
-            input = scanner.nextLine();
-            if("end".equals(input)) {
+            String input = scanner.nextLine();
+
+            if ("end".equals(input)) {
                 break;
             }
-
             int operation = Integer.parseInt(input);
-            switch(operation) {
-                case 1:
-                    System.out.println("Введите сумму дохода: ");
-                    String moneyStr = scanner.nextLine();
-                    int money = Integer.parseInt(moneyStr);
-                    income += money;
-                    break;
-                case 2:
-                    System.out.println("Введите сумму расхода: ");
-                    String spendingStr = scanner.nextLine();
-                    int spending = Integer.parseInt(spendingStr);
-                    expenses += spending;
-                    break;
-                case 3:
-                    int tax = taxIncomeMinusExpenses(income, expenses);
-                    int taxInc = taxIncome(income);
-                    Difference(tax, taxInc);
-                    System.out.println("Ваш налог составит: " + taxIncome(income) + " рублей");
-                    System.out.println("Налог на другой системе: " + taxIncomeMinusExpenses(income, expenses) + " рублей");
-                    System.out.println("Экономия: " + Economy(tax, taxInc) + " рублей");
-                    break;
-            }
 
-        }
+            switch (operation) {
+                    case 1:
+                        System.out.println("Введите сумму дохода: ");
+                        int money = Integer.parseInt(scanner.nextLine());
+                        income += money;
+                        break;
+                    case 2:
+                        System.out.println("Введите сумму расхода: ");
+                        int spending = Integer.parseInt(scanner.nextLine());
+                        expenses += spending;
+                        break;
+                    case 3:
+                        int taxInc = taxIncome(income);
+                        int tax = taxIncomeMinusExpenses(income, expenses);
+
+                        System.out.println("Ваш налог составит: " + taxInc + " рублей (УСН доходы)");
+                        System.out.println("Налог по другой системе: " + tax + " рублей (УСН доходы минус расходы)");
+
+                        Difference(tax, taxInc);
+                        break;
+                    default:
+                        System.out.println("Неверный номер операции.");
+                }
+            }
     }
+
     public static int taxIncomeMinusExpenses(int income, int expenses) {
-        int tax = (income - expenses) * 15 / 100;
-        if (tax >= 0) {
-            return tax;
-        } else {
+        int difference = income - expenses;
+        if (difference < 0) {
             return 0;
         }
+        return (difference * 15) / 100;
     }
+
 
     public static int taxIncome(int income) {
-        int taxInc = (income * 6) / 100;
-        return taxInc;
+        return (income * 6) / 100;
     }
+
 
     public static void Difference(int tax, int taxInc) {
-        if (tax > taxInc) {
-            System.out.println("Мы советуем вам УСН доходы минус расходы");
-        } else if (tax < taxInc) {
+        if (taxInc < tax) {
             System.out.println("Мы советуем вам УСН доходы");
+            System.out.println("Экономия: " + (tax - taxInc) + " рублей");
+        } else if (taxInc > tax) {
+            System.out.println("Мы советуем вам УСН доходы минус расходы");
+            System.out.println("Экономия: " + (taxInc - tax) + " рублей");
         } else {
             System.out.println("Можете выбрать любую систему налогообложения");
-
         }
-    }
-
-    public static int Economy(int tax, int taxInc) {
-        int economy = Math.abs(tax - taxInc);
-        return economy;
     }
 }
